53f6bc34dd2fa678a0b29d3e20fd8e650f76d668
kbajrang <135621480+kbajrang@users.noreply.github.com>
2024-05-31 18:36:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let skeleton;
 
 function setup() {
     let canvas = createCanvas(800, 500);
-    canvas.parent('canvasContainer'); // Ensure the canvas is attached to the correct parent element
+    canvas.parent('canvasContainer'); // Attach canvas to a div
     capture = createCapture(VIDEO);
     capture.size(800, 500);
     capture.hide();
@@ -18,20 +18,10 @@ function setup() {
 }
 
 function receivedPoses(poses) {
-    console.log(poses);
     if (poses.length > 0) {
         singlePose = poses[0].pose;
         skeleton = poses[0].skeleton;
-        noseX = singlePose.nose.x;
-        noseY = singlePose.nose.y;
-
-        reyex = singlePose.rightEye.x;
-        reyey = singlePose.rightEye.y;
-
-        leyex = singlePose.leftEye.x;
-        leyey = singlePose.leftEye.y;
     }
-    console.log(noseX + " " + noseY);
 }
 
 function modelLoaded() {
@@ -40,17 +30,13 @@ function modelLoaded() {
 
 function draw() {
     image(capture, 0, 0, 800, 500);
-    fill(255, 0, 0);
-
     if (singlePose) {
         for (let i = 0; i < singlePose.keypoints.length; i++) {
+            fill(255, 0, 0);
             ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 30);
         }
-    }
-
-    stroke(255, 255, 255);
-    strokeWeight(5);
-    if (skeleton) {
+        stroke(255, 255, 255);
+        strokeWeight(5);
         for (let j = 0; j < skeleton.length; j++) {
             line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y);
         }
